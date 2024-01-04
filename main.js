0b55b90ba7dfc40bc262d779f881ef083f29ce3e
pejuameenat <104262132+pejuameenat@users.noreply.github.com>
2024-01-05 00:21:29 +0100

    development

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-import obj from './obj.js';
+import { obj, obj2  } from "./obj.js";
 
-const cards = document.getElementById('grid');
+const cards = document.getElementById("grid");
+const questions = document.getElementById("faq");
 
-
-function makeCards(){
- let html = '';
-  obj.map(function(item){
-   html+= `<div class="card">
+function makeCards() {
+  let html = "";
+  obj.map(function (item) {
+    html += `<div class="card">
     <a class="card1" href="#">
     <h3>${item.job}</h3>
     <p class="small">${item.desc}</p>
@@ -16,9 +16,49 @@ function makeCards(){
      </div>
    </div>
  </a>
- </div>`
-  })
-  
-return cards.insertAdjacentHTML("afterbegin", html);
+ </div>`;
+  });
+
+  return cards.insertAdjacentHTML("afterbegin", html);
 }
 makeCards();
+
+function faqs() {
+  let elem = "";
+  obj2.map(function (el) {
+    elem += `<fieldset>
+       <div class="questions">
+          <h4>${el.h4}</h4>
+          <p class=" show hidden">${el.p}</p>
+      </div>
+      <img src="./images/plus.png" class="faqimg" alt="plus-sign">
+   </fieldset>`;
+  });
+  return questions.insertAdjacentHTML("afterbegin", elem);
+}
+faqs();
+
+
+questions.addEventListener('click', function(e){
+  let clicked = e.target.closest('fieldset')
+ const closestP = clicked.children[0].lastElementChild;
+ const img = clicked.lastElementChild;
+ if(clicked && closestP.classList.contains('hidden')){
+   closestP.classList.remove('hidden')
+   img.src = './images/minus.png'
+   img.classList.add('fieldset-img')
+}else{
+  closestP.classList.add('hidden');
+  img.src = './images/plus.png'
+}
+})
+
+navigator.geolocation.getCurrentPosition(function(position){
+  console.log(position.coords);
+  const {latitude} = position.coords
+  const {longitude} = position.coords
+  console.log(latitude,longitude);
+  console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+}, function(){
+  alert('not seen')
+})
--- obj.js
+++ obj.js
@@ -1,5 +1,5 @@
 
- const obj = [
+ export const obj = [
     {id: 0,
     job: 'Tailoring',
     desc: 'lorem ipsum dolor sit amet consectier ilium'
@@ -40,6 +40,46 @@
     job: 'Shoe making',
     desc: 'lorem ipsum dolor sit amet consectier ilium'
    },
+   {id: 10,
+      job: 'Electricians',
+      desc: 'lorem ipsum dolor sit amet consectier ilium'
+   },
+   {id: 11,
+      job: 'Vulcanizing',
+      desc: 'lorem ipsum dolor sit amet consectier ilium'
+   },
 ]
 
-export default obj;
+
+
+ export const obj2 = [
+   {id: 0,
+   h4:'how do i request the services of an artisan ?',
+   p: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quaerat numquam non, aperiam laboriosam accusamus?Lorem ipsum dolor sit amet.'
+   },
+   {id: 1,
+      h4:'how do I make payement for the services rendered ?',
+      p: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quaerat numquam non, aperiam laboriosam accusamus?Lorem ipsum dolor sit amet.'
+   },
+   {id: 2,
+      h4:'how do i know an artisan has been verified ?',
+      p: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quaerat numquam non, aperiam laboriosam accusamus?Lorem ipsum dolor sit amet.'
+    },
+   {id: 3,
+      h4:"Will i get a reversal if i'm not satisfied with services rendered ?",
+      p: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quaerat numquam non, aperiam laboriosam accusamus?Lorem ipsum dolor sit amet.'
+   },
+   {id: 4,
+      h4:'how do i verify my account as a vendor ?',
+      p: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quaerat numquam non, aperiam laboriosam accusamus?Lorem ipsum dolor sit amet.'
+      },
+   {id: 5,
+      h4:'how do i request the services of an artisan ?',
+       p: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quaerat numquam non, aperiam laboriosam accusamus?Lorem ipsum dolor sit amet.'
+   },
+   {id: 6,
+      h4:'how do i request the services of an artisan ?',
+      p: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quaerat numquam non, aperiam laboriosam accusamus?Lorem ipsum dolor sit amet.'
+   }
+]
+
